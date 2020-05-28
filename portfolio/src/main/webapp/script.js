616c662953f5fb0a70911ac87ea81d47cdc1a8e1
Keely Wan <keelywan00@gmail.com>
2020-05-28 15:35:58 -0400

    Add travel bucket list gallery images and labels. Implement function so name of location is displayed when image is moused over. Add section tags to divide portfolio into different topic segments.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,22 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Reduces opacity and shows name of location when image is moused over.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+function displayLocation(name) {
+  var label = document.getElementById(name); 
+  var img = document.getElementById(name+'-Img'); 
+  label.style.display = "block"; 
+  img.style.opacity = 0.3; 
 }
+
+/**
+ * Hides label and changes opacity back to 1 when mousing out of image. 
+ */
+
+function hideLocation(name) {
+  var label = document.getElementById(name); 
+  var img = document.getElementById(name+'-Img'); 
+  label.style.display = "none"; 
+  img.style.opacity = 1; 
+}	
