293b9a1d25e367b20065c9ad691c250e25992657
Keely Wan <keelywan00@gmail.com>
2020-06-10 13:16:15 +0000

    Initial outline of calendar algorithm.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,58 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    HashSet<TimeRange> meetingTimes = new HashSet<>();
+    long duration = request.getDuration();
+    if(duration > TimeRange.WHOLE_DAY.duration() || duration < 0) {
+      return meetingTimes;
+    }
+
+    Collection<String> attendees = request.getAttendees();
+    ArrayList<Event> eventsList = new ArrayList<Event>(events);
+    printEventList(eventsList);
+
+    Comparator<Event> compareByStartTime = (Event e1, Event e2) -> e1.getWhen().start() - e2.getWhen().start();
+    Collections.sort(eventsList, compareByStartTime);
+    printEventList(eventsList);
+
+    int curTime = 0;
+    int listIndex = 0;
+    while(curTime + duration < TimeRange.END_OF_DAY && listIndex < eventsList.size()) {
+
+    }
+
+    return meetingTimes;
+    // throw new UnsupportedOperationException("TODO: Implement this method.");
+    // Check time request.getDuration() should be greater than 0, less than 24
+    // Sort events in order of start times -- DONE (does this need to be done?)
+    // Find events with all attendees? Does this need to be done? -- for now, assume events have at least one of the attendees
+    // Merge together adjacent or overlapping events
+    // Find empty blocks 
+    // while time + duration < TimeRange.END_OF_DAY
+  }
+
+  // Merge overlapping events -- only pass if list is greater than 1
+  public ArrayList<Event> mergeEvents(ArrayList<Event> events) {
+    int startTime = events.get(0).getWhen().start();
+    int endTime = events.get(0).getWhen().end();
+    for(int i = 1; i < events.size(); i++) {
+
+    }
+    return null;
+  }
+
+  // Debugging purposes  
+  public void printEventList(ArrayList<Event> events) {
+    for(int i = 0; i < events.size(); i++) {
+      System.out.println(events.get(i).getWhen().start());
+    }
+    System.out.println("-------BREAK-------");
   }
 }
