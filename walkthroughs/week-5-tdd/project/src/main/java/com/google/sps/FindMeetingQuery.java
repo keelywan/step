750e8a8df6d918e7fa29267ee47fd6ded4909bf9
Keely Wan <keelywan00@gmail.com>
2020-06-16 15:13:14 +0000

    Implement meeting query and pass basic tests.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,69 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    ArrayList<TimeRange> meetingTimes = new ArrayList<>();
+    long duration = request.getDuration();
+    if(duration > TimeRange.WHOLE_DAY.duration() || duration < 0) {
+      return meetingTimes;
+    }
+
+    ArrayList<String> attendees = new ArrayList<String>(request.getAttendees());
+    ArrayList<Event> eventsList = new ArrayList<Event>(events);
+
+    Comparator<Event> orderByStartTime =
+        (Event e1, Event e2) -> Long.compare(e1.getWhen().start(), e2.getWhen().start());
+    Collections.sort(eventsList, orderByStartTime);
+    return filterEventsAndGetAvailableTimes(eventsList, attendees, duration);
+  }
+
+  /**
+   * Determine if there is an intersection between event attendees and meeting attendees.
+   */
+  private boolean hasIntersection(ArrayList<String> eventAttendees, ArrayList<String> meetingAttendees) {
+    for(int i = 0; i < meetingAttendees.size(); i++) {
+      if(eventAttendees.contains(meetingAttendees.get(i))) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Filter out events that don't have meeting request attendees in it and return list
+   * of all available TimeRanges longer than duration.
+   */
+  private ArrayList<TimeRange> filterEventsAndGetAvailableTimes(ArrayList<Event> events,
+      ArrayList<String> attendees, long duration) {
+    ArrayList<TimeRange> availableTimes = new ArrayList<TimeRange>();
+    int curTime = 0;
+    for(int i = 0; i < events.size(); i++) {
+      Event event = events.get(i);
+      ArrayList<String> eventAttendees = new ArrayList<String>(event.getAttendees());
+      if(hasIntersection(eventAttendees, attendees)) {
+        if(curTime + duration > TimeRange.END_OF_DAY) {
+          return availableTimes;
+        }
+        TimeRange timeInterval = event.getWhen();
+        int startTime = timeInterval.start();
+        int endTime = timeInterval.end();
+        int startCurDiff = startTime - curTime;
+        if(startCurDiff >= duration) {
+          availableTimes.add(TimeRange.fromStartDuration(curTime, startCurDiff));
+          curTime = endTime;
+        } else {
+          curTime = Math.max(curTime, endTime);
+        }
+      }
+    }
+    if(TimeRange.END_OF_DAY - curTime >= duration) {
+      availableTimes.add(TimeRange.fromStartEnd(curTime, TimeRange.END_OF_DAY, true));
+    }
+    return availableTimes;
   }
 }
